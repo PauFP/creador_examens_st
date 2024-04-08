3a2c3f103cc557654127baa6e3cec66b166d0ecb
unknown <fernandezpujolpau@gmail.com>
2024-04-09 00:50:01 +0200

    hotfix

--- pages/Creador d'Examens.py
+++ pages/Creador d'Examens.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import os
-from fpdf import FPDF
-from datetime import datetime
+# from fpdf import FPDF
 import random
-from PIL import Image
+# from PIL import Image
 import re
 
-class PDF(FPDF):
-    def header(self):
-        self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, 'Examen', 0, 1, 'C')
-
-    def footer(self):
-        self.set_y(-15)
-        self.set_font('Arial', 'I', 8)
-        self.cell(0, 10, f'Página {self.page_no()}', 0, 0, 'C')
+# class PDF(FPDF):
+#     def header(self):
+#         self.set_font('Arial', 'B', 12)
+#         self.cell(0, 10, 'Examen', 0, 1, 'C')
+#
+#     def footer(self):
+#         self.set_y(-15)
+#         self.set_font('Arial', 'I', 8)
+#         self.cell(0, 10, f'Página {self.page_no()}', 0, 0, 'C')
 def get_subjects():
     return os.listdir(subjects_folder)
 
@@ -113,84 +112,84 @@ def display_exercises_with_solutions(subjects_folder, subject, temas_questions, 
                 st.warning(f"No se encontró archivo de solución correspondiente para: {random_problem}")
 
 
-def add_content_to_pdf(pdf, subjects_folder, subject, temas_questions):
-    left_margin, top_margin = 10, 10
-    page_width = 190  # Ancho efectivo de la página ajustando los márgenes
+# def add_content_to_pdf(pdf, subjects_folder, subject, temas_questions):
+#     left_margin, top_margin = 10, 10
+#     page_width = 190  # Ancho efectivo de la página ajustando los márgenes
+#
+#     # Asegurarse de que haya al menos una página en el PDF
+#     if pdf.page_no() == 0:
+#         pdf.add_page()
+#     y_position = top_margin
+#
+#     for tema in temas_questions:
+#         question_paths = []
+#
+#         # Construir la lista completa de posibles preguntas
+#         tema_path = os.path.join(subjects_folder, subject, "Questions", tema)
+#         for root, dirs, files in os.walk(tema_path):
+#             for file in files:
+#                 if "problem" in file:
+#                     question_paths.append(os.path.join(root, file))
+#
+#         if question_paths:
+#             path = random.choice(question_paths)  # Selecciona aleatoriamente una pregunta del tema
+#
+#             if os.path.exists(path):
+#                 img = Image.open(path)
+#                 img_width, img_height = img.size
+#                 aspect_ratio = img_height / img_width
+#                 new_height = page_width * aspect_ratio
+#
+#                 if (y_position + new_height) > (297 - top_margin):  # A4 page height - margin
+#                     pdf.add_page()
+#                     y_position = top_margin  # Restablecer la posición inicial en la nueva página
+#
+#                 pdf.image(path, x=left_margin, y=y_position, w=page_width)
+#                 y_position += new_height + 5  # Actualizar posición Y para la siguiente imagen
+#         else:
+#             st.warning(f"No se encontraron preguntas para el tema: {tema}")
+#
+#     return y_position
 
-    # Asegurarse de que haya al menos una página en el PDF
-    if pdf.page_no() == 0:
-        pdf.add_page()
-    y_position = top_margin
-
-    for tema in temas_questions:
-        question_paths = []
-
-        # Construir la lista completa de posibles preguntas
-        tema_path = os.path.join(subjects_folder, subject, "Questions", tema)
-        for root, dirs, files in os.walk(tema_path):
-            for file in files:
-                if "problem" in file:
-                    question_paths.append(os.path.join(root, file))
-
-        if question_paths:
-            path = random.choice(question_paths)  # Selecciona aleatoriamente una pregunta del tema
-
-            if os.path.exists(path):
-                img = Image.open(path)
-                img_width, img_height = img.size
-                aspect_ratio = img_height / img_width
-                new_height = page_width * aspect_ratio
-
-                if (y_position + new_height) > (297 - top_margin):  # A4 page height - margin
-                    pdf.add_page()
-                    y_position = top_margin  # Restablecer la posición inicial en la nueva página
-
-                pdf.image(path, x=left_margin, y=y_position, w=page_width)
-                y_position += new_height + 5  # Actualizar posición Y para la siguiente imagen
-        else:
-            st.warning(f"No se encontraron preguntas para el tema: {tema}")
-
-    return y_position
-
-def add_problems_to_pdf(pdf, subjects_folder, subject, temas_problemas, start_y_position):
-    left_margin, top_margin = 10, 10
-    page_width = 190  # Ancho efectivo de la página ajustando los márgenes
-
-    # Asegurarse de que haya al menos una página en el PDF
-    if pdf.page_no() == 0:
-        pdf.add_page()
-
-    y_position = start_y_position
-
-    for tema in temas_problemas:
-        problem_paths = []
-
-        # Construir la lista completa de posibles problemas
-        tema_path = os.path.join(subjects_folder, subject, "Problemes", tema)
-        for root, dirs, files in os.walk(tema_path):
-            for file in files:
-                if "problem" in file:
-                    problem_paths.append(os.path.join(root, file))
-
-        if problem_paths:
-            path = random.choice(problem_paths)  # Selecciona aleatoriamente un problema del tema
-
-            if os.path.exists(path):
-                img = Image.open(path)
-                img_width, img_height = img.size
-                aspect_ratio = img_height / img_width
-                new_height = page_width * aspect_ratio
-
-                if (y_position + new_height) > (297 - top_margin):  # Altura de página A4 - margen
-                    pdf.add_page()
-                    y_position = top_margin  # Restablecer la posición inicial en la nueva página
-
-                pdf.image(path, x=left_margin, y=y_position, w=page_width)
-                y_position += new_height + 5  # Actualizar posición Y para la siguiente imagen
-        else:
-            st.warning(f"No se encontraron problemas para el tema: {tema}")
-
-    return y_position
+# def add_problems_to_pdf(pdf, subjects_folder, subject, temas_problemas, start_y_position):
+#     left_margin, top_margin = 10, 10
+#     page_width = 190  # Ancho efectivo de la página ajustando los márgenes
+#
+#     # Asegurarse de que haya al menos una página en el PDF
+#     if pdf.page_no() == 0:
+#         pdf.add_page()
+#
+#     y_position = start_y_position
+#
+#     for tema in temas_problemas:
+#         problem_paths = []
+#
+#         # Construir la lista completa de posibles problemas
+#         tema_path = os.path.join(subjects_folder, subject, "Problemes", tema)
+#         for root, dirs, files in os.walk(tema_path):
+#             for file in files:
+#                 if "problem" in file:
+#                     problem_paths.append(os.path.join(root, file))
+#
+#         if problem_paths:
+#             path = random.choice(problem_paths)  # Selecciona aleatoriamente un problema del tema
+#
+#             if os.path.exists(path):
+#                 img = Image.open(path)
+#                 img_width, img_height = img.size
+#                 aspect_ratio = img_height / img_width
+#                 new_height = page_width * aspect_ratio
+#
+#                 if (y_position + new_height) > (297 - top_margin):  # Altura de página A4 - margen
+#                     pdf.add_page()
+#                     y_position = top_margin  # Restablecer la posición inicial en la nueva página
+#
+#                 pdf.image(path, x=left_margin, y=y_position, w=page_width)
+#                 y_position += new_height + 5  # Actualizar posición Y para la siguiente imagen
+#         else:
+#             st.warning(f"No se encontraron problemas para el tema: {tema}")
+#
+#     return y_position
 
 
 st.title("Creador de Exámenes")
